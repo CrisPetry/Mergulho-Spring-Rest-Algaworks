990aa05838644be40111a10b1e96280d7a9ed023
cristianpetry <cristianpetry@upf.br>
2022-07-21 15:17:12 -0300

    Aula 2 - Configurando o Flyway

--- src/main/java/com/web/nilteklog/api/controller/ClienteController.java
+++ src/main/java/com/web/nilteklog/api/controller/ClienteController.java
@@ -1,25 +1,21 @@
 package com.web.nilteklog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.web.nilteklog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-
-		Cliente client1 = new Cliente();
-		client1.setId(1L);
-		client1.setNome("João");
-		client1.setTelefone("54 99921-2354");
-		client1.setEmail("joao@uol.br");
-
-		return Arrays.asList(client1);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
